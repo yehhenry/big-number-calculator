d1c0b4a58d4f1cfb451cf126203a68593a938593
Anzioh <a0975864887@gmail.com>
2022-05-04 00:57:54 +0800

    infix to postfix

--- big-number-calculator/main.cpp
+++ big-number-calculator/main.cpp
@@ -1,9 +1,103 @@
 ﻿#include <iostream>
 #include <vector>
+#include <string>
+#include <stack>
 #include "Blamath.h"
 #include "Decimal.h"
 #include "Integer.h"
 using namespace std;
+
+vector<string> res;
+
+int prec(string c) {
+	if (c == "!") {
+		return 5;
+	}
+	else if (c == "^") {
+		return 4;
+	}
+	else if (c == "*") {
+		return 3;
+	}
+	else if (c == "/") {
+		return 2;
+	}
+	else if (c == "+" || c == "-") {
+		return 1;
+	}
+	else {
+		return -1;
+	}
+}
+
+int precc(char c)
+{
+	if (c == '!') {
+		return 5;
+	}
+	else if (c == '^') {
+		return 4;
+	}
+	else if (c == '*') {
+		return 3;
+	}
+	else if (c == '/') {
+		return 2;
+	}
+	else if (c == '+' || c == '-') {
+		return 1;
+	}
+	else {
+		return -1;
+	}
+}
+
+void infixToPostfix(string str) {
+	stack<string> st;
+	string result;
+
+	for (int i = 0; i < str.length(); i++) {
+		char c = str[i];
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+			while ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+				result = result + c;
+				i++;
+				c = str[i];
+			}
+			res.push_back(result);
+			result.clear();
+		}
+		if (c == '(') {
+			st.push("(");
+		}
+		else if (c == ')') {
+			while (st.top() != "(") {
+				res.push_back(st.top());
+				st.pop();
+			}
+			st.pop();
+		}
+		else {
+			while (!st.empty() && precc(str[i]) <= prec(st.top())) {
+				if (c == '!' && st.top() == "!") {
+					break;
+				}
+				else {
+					res.push_back(st.top());
+					st.pop();
+				}
+			}
+			result = c;
+			st.push(result);
+			result.clear();
+		}
+	}
+
+	while (!st.empty()) {
+		res.push_back(st.top());
+		st.pop();
+	}
+}
 
 int main() {
 	Blamath a(25);
