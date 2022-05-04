66e052158161bd009f39527bd0384ea15a9bd6e6
Anzioh <a0975864887@gmail.com>
2022-05-04 16:26:53 +0800

    weight fix

--- big-number-calculator/main.cpp
+++ big-number-calculator/main.cpp
@@ -11,15 +11,12 @@ vector<string> res;
 
 int prec(string c) {
 	if (c == "!") {
-		return 5;
-	}
-	else if (c == "^") {
 		return 4;
 	}
-	else if (c == "*") {
+	else if (c == "^") {
 		return 3;
 	}
-	else if (c == "/") {
+	else if (c == "*" || c == "/") {
 		return 2;
 	}
 	else if (c == "+" || c == "-") {
@@ -33,15 +30,12 @@ int prec(string c) {
 int precc(char c)
 {
 	if (c == '!') {
-		return 5;
-	}
-	else if (c == '^') {
 		return 4;
 	}
-	else if (c == '*') {
+	else if (c == '^') {
 		return 3;
 	}
-	else if (c == '/') {
+	else if (c == '*' || c == '/') {
 		return 2;
 	}
 	else if (c == '+' || c == '-') {
@@ -97,12 +91,77 @@ void infixToPostfix(string str) {
 		res.push_back(st.top());
 		st.pop();
 	}
+
 }
+
+string calculate(Blamath a, Blamath b, const std::string operatorSign)
+{
+	Blamath temp;
+	if (operatorSign == "+") {
+		temp = a + b;
+		return temp.toString();
+	}
+	else if (operatorSign == "-") {
+		temp = a - b;
+		return temp.toString();
+	}
+	else if (operatorSign == "*") {
+		temp = a * b;
+		return temp.toString();
+	}
+	else if (operatorSign == "/") {
+		temp = a / b;
+		return temp.toString();
+	}
+	else if (operatorSign == "^") {
+		temp = a ^ b;
+		return temp.toString();
+	}
+
+}
+// POSTFIX evaluation to get result
+static std::string evaluatePostfixExpression()
+{
+	stack <string> myStack;
+	string str;
+	for (int i = 0, j = res.size() - 1,index=0; i < j; i++,index++)
+	{
+		str = res[index];
+		if (index + 1 <= j) {
+			if (res[index + 1] == "!") {
+				i--;
+			}
+		}
+		
+		if (str != "!" && str != "^" && str != "*" && str != "/" && str != "+" && str != "-") {
+			myStack.push(res[index]);
+		}
+		else if (str == "!") {
+			Blamath a(myStack.top());
+			myStack.pop();
+			myStack.push(a.getFactorial().toString());
+		}
+		else if (str == "^" || str == "*" || str == "/" || str == "+" || str == "-") //found operator
+		{
+
+			Blamath b(myStack.top());
+			myStack.pop();
+
+			Blamath a(myStack.top());
+			myStack.pop();
+
+			myStack.push(calculate(a, b, str));
+		}
+	}
+	return myStack.top();
+}
+
 
 int main() {
 	Blamath a(25);
+	string ans;
 	//			   階層↓
-	std::cout << a.getFactorial().toString() << endl;
+	/*std::cout << a.getFactorial().toString() << endl;
 
 	Integer i("123");
 	Decimal d("123.3");
@@ -112,5 +171,20 @@ int main() {
 	nums.push_back(&d);
 	for (const auto& num : nums) {
 		cout << *num << endl;
+	}*/
+	string infix;
+	while (getline(cin, infix)) {
+		infixToPostfix(infix);
+
+		cout << "Infix Expression: " << infix << endl;
+		cout << "Postfix Expression: ";
+		//verify result
+		for (int i = 0; i < res.size(); i++) {
+			cout << res[i];
+		}
+		cout << endl;
+		ans = evaluatePostfixExpression();
+		cout << ans << endl;
+		res.clear();
 	}
 }
